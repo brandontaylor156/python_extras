8ba961c4b96ee1f863ed9186ba99708e2e7f2f89
brandontaylor156 <brandon.david4@outlook.com>
2022-07-06 00:37:57 -0700

    finished

--- singly_linked_lists.py
+++ singly_linked_lists.py
@@ -1,6 +1,3 @@
-from os import remove
-
-
 class SList:
     def __init__(self):
         self.head = None
@@ -24,6 +21,30 @@ class SList:
         runner.next = newNode
         return self
 
+    def insert_at(self, value, n):
+        counter = 1
+        if n == 0:
+            self.add_to_front(value)
+            return self
+        else:
+            runner = self.head
+            while(runner.next.next != None):
+                if counter == n:
+                    newNode = SLNode(value)
+                    newNode.next = runner.next
+                    runner.next = newNode
+                    return self
+                counter += 1
+                runner = runner.next
+            if counter == n:
+                newNode = SLNode(value)
+                newNode.next = runner.next
+                runner.next = newNode
+            elif counter + 1 == n:
+                self.add_to_back(value)
+            return self
+
+
     def remove_from_front(self):
         if self.head != None:
             if self.head.next == None:
@@ -45,8 +66,20 @@ class SList:
         return self
 
     def remove_val(self, value):
-        if self.head.value == value:
+        if self.head == None:
+            return self
+        elif self.head.value == value:
             self.remove_from_front()
+        else:
+            runner = self.head
+            while (runner.next.next != None):
+                if (runner.next.value == value):
+                    runner.next = runner.next.next
+                    return self
+                runner = runner.next
+            if runner.next.value == value:
+                self.remove_from_back()
+               
         return self
         
 
@@ -63,6 +96,6 @@ class SLNode:
         self.next = None
 
 my_list = SList()
-my_list.add_to_front("are").add_to_front("Linked lists").add_to_back("fun!").print_values()
+my_list.add_to_front("are").add_to_front("Linked lists").add_to_back("fun!").add_to_front("Hello").add_to_back("Yay")
 
-my_list.remove_val("Linked lists").print_values()
+my_list.remove_val("Yay").insert_at("Why", ).print_values()
